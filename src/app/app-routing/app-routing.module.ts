9739fa5604783011b067144963bb66c3bde7b034
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-07-02 14:54:15 -0300

    authguard created

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../auth/auth.guard';
 import { WelcomeComponent } from '../welcome/welcome.component';
 import { SignupComponent } from '../auth/signup/signup.component';
 import { LoginComponent } from '../auth/login/login.component';
@@ -11,7 +12,7 @@ const routes: Routes = [
   { path: '', component: WelcomeComponent},
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'training', component: TrainingComponent },
+  { path: 'training', component: TrainingComponent, canActivate: [AuthGuard] },
 ]
 
 @NgModule({
@@ -21,6 +22,7 @@ const routes: Routes = [
   exports: [ 
     RouterModule
   ],
+  providers: [AuthGuard],
   declarations: []
 })
 export class AppRoutingModule { }
---
+++ src/app/auth/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable( )
+
+export class AuthGuard implements CanActivate {
+
+  constructor( private authService: AuthService,
+    private router: Router ) {
+
+  }
+  canActivate( route: ActivatedRouteSnapshot, state: RouterStateSnapshot ): boolean {
+    if( this.authService.isAuth( ) ){
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+    }
+  }
+}

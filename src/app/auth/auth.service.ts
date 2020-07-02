68b0944c2ea5f8957bdb88e90f831275dbc52dbd
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-07-02 12:41:40 -0300

    documentation

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,29 @@
+/**
+ * This service is mocking the responses of an authentication backend.
+ * I'm using a Subject to emit authentication changes events
+ */
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from './user.module';
 import { AuthData } from './auth-data.module';
 import { Subject } from 'rxjs';
 
+@Injectable()
+
 export class AuthService {
   authChange = new Subject<boolean>( );
   private user: User;
+
+  constructor( private router: Router ) {
+
+  }
 
   registerUser( authData: AuthData ) {
     this.user = {
       email: authData.email,
       userId: Math.round( Math.random() * 10000 ).toString( )
     }
-    console.log(this.user);
-    this.authChange.next( true );
+    this.loginSuccess( );
   }
 
   loginUser( authData: AuthData ) {
@@ -20,16 +31,16 @@ export class AuthService {
       email: authData.email,
       userId: Math.round( Math.random() * 10000 ).toString( )
     }
-    console.log("Loged user:");
-    console.log(this.user);
-    this.authChange.next( true );
+    this.loginSuccess( );
   }
 
   logout( ) {
+    //when user logs out we redirect it to home page.
     this.user = null;
     console.log("User logged out:");
     console.log(this.user);
     this.authChange.next( false );
+    this.router.navigate( [''] );
   }
 
   getUser( ) {
@@ -39,4 +50,13 @@ export class AuthService {
   isAuth( ) {
       return this.user != null;
   }
+
+  private loginSuccess( ) {
+    //when user logs in we redirect it to training page.
+    this.authChange.next( true );
+    console.log("Loged user:");
+    console.log(this.user);
+    this.router.navigate( ['/training'] );
+  }
+
 }

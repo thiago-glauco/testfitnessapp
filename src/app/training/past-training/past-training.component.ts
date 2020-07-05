4ed09149b57e781173192d1c4b1acbc49c04d9ae
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-07-05 18:50:01 -0300

    2 files modified

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TrainingService } from '../training.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-past-training',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PastTrainingComponent implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['date', 'name'];
+  pastExercisesSubscription: Subscription;
+  dataSource; 
+
+  constructor(
+    private trainingService: TrainingService
+  ) { }
 
   ngOnInit() {
+    this.pastExercisesSubscription = this.trainingService
+      .completedExercisesSubject.subscribe( (exercises) => this.dataSource = exercises );
+  }
+
+  ngOnDestroy( ) {
+    this.pastExercisesSubscription.unsubscribe( );
   }
 
 }

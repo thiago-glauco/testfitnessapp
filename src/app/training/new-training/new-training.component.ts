f1a17541b0fdc3e6278f1bdaccd80b2396bf0646
thiago-glauco <thiagoglauco@yahoo.com.br>
2020-07-18 21:53:50 -0300

    3 files modified

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy, EventEmitter, Output } from '@angular/core';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
+import { UiService } from '../../shared/ui.service';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -15,15 +16,22 @@ export class NewTrainingComponent implements OnInit {
   exercises:Exercise[ ];
   exerciseSubscription: Subscription = new Subscription( );
   selectedExercise: string = '';
+  waitingExercisesSubscription: Subscription;
+  hasLoadedExercises: boolean = false;
 
   @Output( ) trainingStart = new EventEmitter( );
 
   constructor( 
     private trainingService: TrainingService,
-    private angularFirestore: AngularFirestore ) { }
+    private angularFirestore: AngularFirestore,
+    private uiService: UiService ) { }
 
   ngOnInit() {
     this.trainingService.fetchExercises( );
+    this.waitingExercisesSubscription = this.uiService
+      .waitDatabaseSubscription.subscribe( ( loaded: boolean ) => {
+        this.hasLoadedExercises = loaded
+      });
     this.exerciseSubscription = this.trainingService
       .exercisesChanged
       .subscribe( exercises => this.exercises = exercises );
